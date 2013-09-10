bfb255882dc51a6bb97c40f4c94730c970e851c7
David Lichtenberg <david.m.lichtenberg@gmail.com>
2013-09-09 23:07:10 -0400

    ui now includes user print output and error help for user

--- ui.py
+++ ui.py
@@ -1,63 +1,73 @@
 import re
-import sys
+from error_messages import error_messages
 from library import Library
+
+def find_quoted(string):
+    """returns list of substrings contained in double quotes"""
+    return re.findall(r'\"(.+?)\"', string)
 
 class LibraryTerminalInterface(object):
     def __init__(self):
         self.library = Library()
+        self.commands = {}
 
     def run(self):
         print "Welcome to your library!"
         while True:
-            user_input = sys.stdin.readline()
-            #TODO: ADD TRY BLOCK
-            action, args = self.parse_query(user_input)
-            if action == 'quit':
-                break
-            else:
-                self.execute(action, args)
+            user_input = raw_input('> ')
+            try:
+                action, args = self.parse_query(user_input)
+                if action != 'quit':
+                    self.execute(action, args)
+                else: 
+                    break
+            except Exception, e:
+                print error_messages.get(str(e), e)
 
     def parse_query(self, input_string):
-        #TODO: push actions definition to decorators
-        parsers = {
-            'show': self.parse_show,
-            'add': self.parse_add,
-            'read': self.parse_read
-        }
+        """takes user input string
+        returns tuple containing action and list of arguments"""
         action, arg_string = input_string.split(" ", 1)
-        parser = parsers.get(action, False)
-        args = (parser and parser(arg_string)) or []
-        return action, args
+        sub_parser = 'parse_' + action
+        args = []
+        if action == 'quit':
+            return action, args
+        elif self.has_method(sub_parser):
+            exec 'args = self.' + sub_parser + '(arg_string)'
+            return action, args
+        else:
+            raise Exception('BAD_ACTION')
+
+    def has_method(self, name):
+        """returns true if object contains method with name"""
+        return name in dir(self) and callable(getattr(self, name))
 
     def parse_add(self, arg_string):
-        """returns an ordered list containing title and author
-        throws exception if input string is bad"""
-        quote_terms = re.findall(r'\"(.+?)\"', arg_string)
+        """returns an ordered list containing title and author"""
+        quote_terms = find_quoted(arg_string)
         if len(quote_terms) == 2:
             return quote_terms
-        raise Exception('BAD ADD INPUT')
+        raise Exception('BAD_ADD_INPUT')
 
     def parse_show(self, arg_string):
         """returns list containing state of books to return
         and author to filter by if author supplied in arg string"""
         tokens = arg_string.split()
-        all_or_unread = tokens[0]
-        returns = []
-        returns.append(all_or_unread)
-        if all_or_unread not in ['all', 'unread']:
-            raise Exception('BAD SHOW INPUT')
+        args = [tokens[0]]
+        if args[0] not in ['all', 'unread']:
+            raise Exception('BAD_SHOW_INPUT')
         if len(tokens) > 1:
-            quote_terms = re.findall(r'\"(.+?)\"', arg_string)
+            quote_terms = find_quoted(arg_string)
             if ('by' not in arg_string) or (len(quote_terms) is not 1):
-                raise Exception('BAD SHOW INPUT')
+                raise Exception('BAD_SHOW_INPUT')
             author = quote_terms[0]
-            returns.append(author)
-        return returns
+            args.append(author)
+        return args
 
     def parse_read(self, arg_string):
         """returns a list containing only title
         throws exception if input string is bad"""
-        quote_terms = re.findall(r'\"(.+?)\"', arg_string)
+        quote_terms =  find_quoted(arg_string)
         if len(quote_terms) == 1:
             if len(quote_terms[0])+2 != len(arg_string):
                 raise Exception('BAD_READ_INPUT')
@@ -67,4 +77,14 @@ class LibraryTerminalInterface(object):
 
     def execute(self, action, args):
         """executes given action with args list"""
-        exec 'result = self.library.' + action + '(*args)'
+        if action == 'add':
+            self.library.add(*args)
+            print 'Added "{title}" by {author}'.format(
+                title = args[0],
+                author = args[1])
+        elif action == 'read':
+            self.library.read(*args)
+            print "You've read \"{title}!\"".format(title=args[0])
+        elif action == 'show':
+            for book in self.library.show(*args):
+                print book
