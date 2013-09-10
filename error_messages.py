86bb529159f826e5054a2b6e68966b9a64dc824c
David Lichtenberg <david.m.lichtenberg@gmail.com>
2013-09-09 23:08:10 -0400

    created error messages dictionary

---
+++ error_messages.py
@@ -0,0 +1,21 @@
+error_messages = {
+    'BAD_ACTION': "error with first term in query - please user add, read, show, or quit",
+    'BAD_ADD_INPUT': """
+    error with input supplied for add statement, please format:
+    add "<title>" "<author>"
+    where <title> and <author> correspond to book info
+    example: add "The Grapes of Wrath" "John Steinbeck"
+    """,
+    'BAD_READ_INPUT': """
+    error with input supplied for read statement, please format:
+    read "<title>"
+    where <title> corresponds to book to read
+    """,
+    'BAD_SHOW_INPUT': """
+    error with input supplied for show statement, please format:
+    show <all || unread> [by <author>]
+    where user chooses between all and unread and by author is optional
+    example: show unread by "John Steinbeck"
+    example: show all
+    """
+}
